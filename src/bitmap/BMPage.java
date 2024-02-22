45e24776c5ca69c61c50e67014f62b75e45b3374
StevenChiu2018 <sam.200846@gmail.com>
2024-02-22 01:52:31 -0700

    Do implementation

--- src/bitmap/BMPage.java
+++ src/bitmap/BMPage.java
@@ -1,8 +1,25 @@
 package bitmap;
 
+import java.io.IOException;
 import diskmgr.Page;
+import global.Convert;
 
-public class BMPage extends Page {
+interface ConstSlot {
+    int INVALID_SLOT = -1;
+    int EMPTY_SLOT = -1;
+}
+
+
+public class BMPage extends Page implements ConstSlot {
+    public static final int SIZE_OF_SLOT = 4;
+    public static final int DPFIXED = 4 * 2 + 3 * 4;
+
+    public static final int SLOT_CNT = 0;
+    /**
+     * number of slots in use
+     */
+    private short slotCnt;
+
     /**
      * Default constructor
      */
@@ -21,8 +38,25 @@ public class BMPage extends Page {
      * Determining if the page is empty
      *
      * @return true if page is empty.
+     * @exception java.io.IOException I/O errors
      */
-    public boolean empty() {
+    public boolean empty() throws IOException {
+        slotCnt = Convert.getShortValue(SLOT_CNT, data);
 
+        for (int i = 0; i < slotCnt; i++) {
+            short slot_length = getSlotLength(i);
+
+            if (slot_length != EMPTY_SLOT) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private short getSlotLength(int slotno) throws IOException {
+        int position = DPFIXED + slotno * SIZE_OF_SLOT;
+
+        return Convert.getShortValue(position, data);
     }
 }
