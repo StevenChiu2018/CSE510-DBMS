c9fee1da5c82118c8e3068ebcef681b32353ee0b
StevenChiu2018 <sam.200846@gmail.com>
2024-02-22 00:41:10 -0700

    Implementation

--- src/bitmap/BMPage.java
+++ src/bitmap/BMPage.java
@@ -1,8 +1,19 @@
 package bitmap;
 
+import java.io.IOException;
 import diskmgr.Page;
+import global.*;
 
 public class BMPage extends Page {
+    public static final int SIZE_OF_SLOT = 4;
+
+    private static final int FREE_SPACE = 4;
+
+    /**
+     * number of bytes free in data[]
+     */
+    private short freeSpace;
+
     /**
      * Default constructor
      */
@@ -15,5 +26,16 @@ public class BMPage extends Page {
      */
     public BMPage(Page page) {
         data = page.getpage();
+    }
+
+    /**
+     * Returns the amount of available space on the page.
+     *
+     * @return the amount of available space on the page.
+     * @exception IOException I/O errors
+     */
+    public int available_space() throws IOException {
+        freeSpace = Convert.getShortValue(FREE_SPACE, data);
+        return (freeSpace - SIZE_OF_SLOT);
     }
 }
