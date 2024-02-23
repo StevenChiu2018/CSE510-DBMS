99658fdadae887459e05ce97343c5115277f60a9
楊鈞智 <kenyang@yangjunzhideMacBook-Pro.local>
2024-02-23 15:21:20 -0700

    Completed getNext method under TupleScan class

--- src/columnar/TupleScan.java
+++ src/columnar/TupleScan.java
@@ -67,7 +67,50 @@ public class TupleScan implements GlobalConst {
    */
   public Tuple getNext(TID tid) {
 
+    Tuple tuple = new Tuple(cf.tupleLength);
+    short[] fieldsOffset = new short[cf.getTupleCnt()];
 
+    for (int i = 0, offset = 0; i < cf.numColumns; i++) {
+
+      if(cf.attributeType[i].attrType == AttrType.attrInteger) {
+        fieldsOffset[i] = (short) offset;
+        offset = offset + 4;
+      }
+      if (cf.attributeType[i].atttrType == AttrType.attrString) {
+        fieldsOffset[i] = (short) offset;
+        offset += offset + cf.stringSize;
+      }
+    }
+
+    try {
+      tuple.setTupleMetaData(cf.tupleLength, (short)cf.numColumns, fieldsOffset);
+      int i = 0;
+
+      for (Scan hf: scan) {
+        if(cf.attributeType[i].attrType == AttrType.attrInteger) {
+          Tuple t = hf.getNext(tid.recordIDs[i]);
+          if(t == null) {
+            return null;
+          }
+          t.setTupleMetaData(4, (short)1, fieldsOffset);
+          tuple.setIntFld(i + 1, t.getIntFld(1));
+        }
+
+        if(cf.attributeType[i].attrType == AttrType.attrString) {
+          Tuple t = hf.getNext(tid.recordIDs[i]);
+          if(t == null) {
+            return null;
+          }
+          t.setTupleMetaData(cf.stringSize, (short)1, fieldsOffset);
+          tuple.setIntFld(i + 1, t.getIntFld(1));
+        }
+        i++;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return tuple;
   }
 
 
