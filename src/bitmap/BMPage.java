c1943031cc156f4462658bafe5d34c4a86b61765
StevenChiu2018 <sam.200846@gmail.com>
2024-02-25 19:28:25 -0700

    Do dumpPage

--- src/bitmap/BMPage.java
+++ src/bitmap/BMPage.java
@@ -1,8 +1,43 @@
 package bitmap;
 
+import java.io.IOException;
 import diskmgr.Page;
+import global.Convert;
+import global.PageId;
 
 public class BMPage extends Page {
+    public static final int SIZE_OF_SLOT = 4;
+    public static final int DPFIXED = 4 * 2 + 3 * 4;
+
+    public static final int SLOT_CNT = 0;
+    public static final int USED_PTR = 2;
+    public static final int FREE_SPACE = 4;
+    public static final int NEXT_PAGE = 12;
+    public static final int CUR_PAGE = 16;
+    /**
+     * number of slots in use
+     */
+    private short slotCnt;
+
+    /**
+     * offset of first used byte by data records in data[]
+     */
+    private short usedPtr;
+
+    /**
+     * number of bytes free in data[]
+     */
+    private short freeSpace;
+    /**
+     * forward pointer to data page
+     */
+    private PageId nextPage = new PageId();
+
+    /**
+     * page number of this page
+     */
+    protected PageId curPage = new PageId();
+
     /**
      * Default constructor
      */
@@ -15,5 +50,35 @@ public class BMPage extends Page {
      */
     public BMPage(Page page) {
         data = page.getpage();
+    }
+
+    /**
+     * Dump contents of a page
+     *
+     * @exception IOException I/O error
+     */
+
+    public void dumpPage() throws IOException {
+        int length, offset;
+
+        curPage.pid = Convert.getIntValue(CUR_PAGE, data);
+        nextPage.pid = Convert.getIntValue(NEXT_PAGE, data);
+        usedPtr = Convert.getShortValue(USED_PTR, data);
+        freeSpace = Convert.getShortValue(FREE_SPACE, data);
+        slotCnt = Convert.getShortValue(SLOT_CNT, data);
+
+        System.out.println("dumpPage");
+        System.out.println("curPage= " + curPage.pid);
+        System.out.println("nextPage= " + nextPage.pid);
+        System.out.println("usedPtr= " + usedPtr);
+        System.out.println("freeSpace= " + freeSpace);
+        System.out.println("slotCnt= " + slotCnt);
+
+        for (int i = 0, n = DPFIXED; i < slotCnt; n += SIZE_OF_SLOT, i++) {
+            length = Convert.getShortValue(n, data);
+            offset = Convert.getShortValue(n + 2, data);
+            System.out.println("slotNo " + i + " offset= " + offset);
+            System.out.println("slotNo " + i + " length= " + length);
+        }
     }
 }
