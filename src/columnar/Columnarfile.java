953816d98a3528c81dbf5381585eca811e2a34ff
nemuzard <nemuzard@gmail.com>
2024-03-06 19:35:40 -0700

    updated

--- src/columnar/Columnarfile.java
+++ src/columnar/Columnarfile.java
@@ -7,56 +7,70 @@ import global.*;
 class Columnarfile {
     private static int numColumns;
     private AttrType[] type;
-    public Columnarfile(String name, int numColumns, AttrType[] type )  throws IOException,HFException, HFBufMgrException, HFDiskMgrException{
+    private Heapfile[] heapfiles;
+    private String name;
+    public Columnarfile(String name, int numColumns, AttrType[] type ) throws IOException, HFException, HFBufMgrException, HFDiskMgrException, SpaceNotAvailableException, InvalidSlotNumberException, InvalidTupleSizeException {
+        this.numColumns = numColumns;
+        this.type = type;
+        this.name = name;
+        this.heapfiles = new Heapfile[numColumns];
 
         if(!isFileExist(name+".hdr")){
             //create
-            this.numColumns = numColumns;
-            this.type = type;
-            createHeaderFile(name,numColumns,type);
+            createHeaderFile();
             for(int i = 0; i<numColumns;i++){
-                createHeapFile(name+"."+i);
+                heapfiles[i] = new Heapfile(name+"."+i);
             }
         }else{
             //load
-            loadHeaderFile(name);
+            loadHeaderFile();
         }
 
     }
 
     private boolean isFileExist(String name){
-        File f = new File(name);
-        return f.exists();
-    }
-
-    private void loadHeaderFile(String name){
-
-
-    }
-
-    private void createHeapFile(String name){
         try{
-            File f = new File(name);
-            if(!f.exists()){
-                boolean created = f.createNewFile();
-                if(!created){
-                    System.err.println("Failed to create file for " + name);
-                }
-            }
-        }catch(IOException e){
-            e.printStackTrace();
+            Heapfile f = new Heapfile(name);
+            return true;
+        }catch(Exception e){
+            return false;
         }
     }
 
-    private void createHeaderFile(String name, int numColumns, AttrType[] type){
-        try(FileWriter wr = new FileWriter(name+".hdr")){
-            wr.write("numColumns = "+ numColumns+"\n");
-            for(int i = 0; i <numColumns;i++){
-                wr.write("type"+i+"="+ type[i].toString() + "\n");
+    private void loadHeaderFile() throws HFDiskMgrException, HFException, HFBufMgrException, IOException, InvalidTupleSizeException {
+        Heapfile headerfile = new Heapfile(this.name+".hdr");
+        Scan sc = headerfile.openScan();
+        RID rid = new RID();
+        Tuple tuple;
+        if((tuple=sc.getNext(rid))!= null){
+            byte[] data = tuple.getTupleByteArray();
+            numColumns = Convert.getIntValue(0,data);
+            this.type=new AttrType[numColumns];
+            for(int i = 0; i<numColumns;i++){
+                int attrType = Convert.getIntValue(4 + i * 4, data);
+                this.type[i] = new AttrType(attrType);
             }
-        }catch(IOException e){
-            e.printStackTrace();
         }
+
+        sc.closescan();
+        this.heapfiles = new Heapfile[numColumns];
+        for(int i = 0; i<numColumns;i++){
+            this.heapfiles[i] = new Heapfile(this.name+"." + i);
+        }
+
+
+    }
+
+
+    private void createHeaderFile() throws IOException, HFDiskMgrException, HFException, HFBufMgrException, SpaceNotAvailableException, InvalidSlotNumberException, InvalidTupleSizeException {
+        Heapfile hdrf = new Heapfile(this.name+".hdr");
+        byte[] metaByte = new byte[4+4*numColumns];
+        Convert.setIntValue(numColumns,0,metaByte);
+        for(int i = 0; i < numColumns; i++){
+            Convert.setIntValue(type[i].attrType,4+i*4,metaByte);
+        }
+
+        hdrf.insertRecord(metaByte);
     }
 
 
