85eb9c9e910e245c61eb1a0fd5f904f9046adfb3
Moechinehaa <jessica.ready.win@gmail.com>
2024-03-08 16:25:35 -0700

    not test yet

--- src/diskmgr/ColumnDB.java
+++
@@ -1,8 +0,0 @@
-/* File ColumnDB.java */
-
-package diskmgr;
-
-public class ColumnDB extends DB {
-  /** default constructor. */
-  public ColumnDB() {}
-}
--- src/global/ValueClass.java
+++
@@ -1,5 +0,0 @@
-package global;
-
-public abstract class ValueClass {
-    public ValueClass() {}
-}
