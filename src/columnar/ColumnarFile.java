5a96413cd1377b78b737fd69cbddb2157531514b
楊鈞智 <kenyang@yangjunzhideMacBook-Pro.local>
2024-03-10 23:08:52 -0700

    Revised comment on getValue function under ColumnarFile class.

--- src/columnar/ColumnarFile.java
+++ src/columnar/ColumnarFile.java
@@ -139,9 +139,9 @@ class Columnarfile {
     return t;
   }
 
-  //Read the value with the given column and  tid from the columnar file
+  // Read the value with the given column and  tid from the columnar file
   public ValueClass getValue(TID tid, int column) {
-    
+
     ValueClass value = null;
 		IntegerValue integer = new IntegerValue();
 		StringValue str = new StringValue();
