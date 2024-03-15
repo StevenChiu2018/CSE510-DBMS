195ee56624845aaf8f6125979b1b4a59a69b1c32
蔡承諺 <ym851010@gmail.com>
2024-03-15 09:51:45 -0700

    feat: First draft of columnarFileScan

---
+++ src/index/ColumnarIndexScan.java
@@ -0,0 +1,248 @@
+package index;
+
+import btree.*;
+import bufmgr.*;
+import diskmgr.*;
+import global.*;
+import heap.*;
+import iterator.*;
+import java.io.*;
+import bitmap.BitMapFile;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Index Scan iterator will directly access the required tuple using the provided key. It will also
+ * perform selections and projections. information about the tuples and the index are passed to the
+ * constructor, then the user calls <code>get_next()</code> to get the tuples.
+ */
+public class IndexScan extends Iterator {
+  public FldSpec[] perm_mat;
+  private BitMapFile[] BMFiles;
+  private int[] BMScan;
+  private AttrType[] _types;
+  private short[] _s_sizes;
+  private CondExpr[] _selects;
+  private int _noInFlds;
+  private int _noOutFlds;
+  private Heapfile f;
+  private ColumnarFile columnarFile;
+  private Tuple tuple1;
+  private Tuple Jtuple;
+  private int t1_size;
+  private int [] _fldNum;
+  private boolean index_only;
+  private int scanIndex;
+  private Scan tidHeapFile;
+  private ArrayList<Integer> distinctColPos;
+
+
+  /**
+   * class constructor. set up the index scan.
+   *
+   * @param relName name of the input relation
+   * @param fldNum array of field number of the indexed field
+   * @param index array of type of the index (B_Index, Hash)
+   * @param indName array of name of the input index
+   * @param types array of types in this relation
+   * @param str_sizes array of string sizes (for attributes that are string)
+   * @param noInFlds number of fields in input tuple
+   * @param noOutFlds number of fields in output tuple
+   * @param outFlds fields to project
+   * @param selects conditions to apply, first one is primary
+   * @param indexOnly whether the answer requires only the key or the tuple
+   * @exception IndexException error from the lower layer
+   * @exception InvalidTypeException tuple type not valid
+   * @exception InvalidTupleSizeException tuple size not valid
+   * @exception UnknownIndexTypeException index type unknown
+   * @exception IOException from the lower layer
+   */
+  public IndexScan(
+      final String relName,
+      final int[] fldNum,
+      IndexType[] index,
+      final String[] indName,
+      AttrType types[],
+      short str_sizes[],
+      int noInFlds,
+      int noOutFlds,
+      FldSpec outFlds[],
+      CondExpr selects[],
+      final boolean indexOnly)
+      throws IndexException,
+          InvalidTypeException,
+          InvalidTupleSizeException,
+          UnknownIndexTypeException,
+          IOException {
+    this._fldNum = fldNum;
+    this._noInFlds = noInFlds;
+    this._types = types;
+    this._s_sizes = str_sizes;
+
+    AttrType[] Jtypes = new AttrType[noOutFlds];
+    short[] ts_sizes;
+    this.Jtuple = new Tuple();
+
+    try {
+      ts_sizes =
+          TupleUtils.setup_op_tuple(this.Jtuple, Jtypes, types, noInFlds, str_sizes, outFlds, noOutFlds);
+    } catch (TupleUtilsException e) {
+      throw new IndexException(
+          e, "IndexScan.java: TupleUtilsException caught from TupleUtils.setup_op_tuple()");
+    } catch (InvalidRelation e) {
+      throw new IndexException(
+          e, "IndexScan.java: InvalidRelation caught from TupleUtils.setup_op_tuple()");
+    }
+
+    this._selects = selects;
+    this.perm_mat = outFlds;
+    this._noOutFlds = noOutFlds;
+    this.tuple1 = new Tuple();
+    try {
+      this.tuple1.setHdr((short) noInFlds, types, str_sizes);
+    } catch (Exception e) {
+      throw new IndexException(e, "IndexScan.java: Heapfile error");
+    }
+
+    this.t1_size = this.tuple1.size();
+    this.index_only = indexOnly; // added by bingjie miao
+
+    try {
+      this.columnarFile = new ColumnarFile(this.relName);
+      //f = new Heapfile(this.relName);
+    } catch (Exception e) {
+      throw new IndexException(e, "IndexScan.java: Heapfile not created");
+    }
+
+    switch (index.indexType) {
+        // Only bitmap is implemented
+      case IndexType.BitMap:
+        // error check the select condition
+        // must be of the type: value op symbol || symbol op value
+        // but not symbol op symbol || value op value
+        try {
+          for(int i = 0; i < indName.length; i++) {
+            this.BMFiles[i] = new BitMapFile(indName[i]);
+          }
+        } catch (Exception e) {
+          throw new IndexException(
+              e, "IndexScan.java: BTreeFile exceptions caught from BTreeFile constructor");
+        }
+
+        try {
+          // Get all positions with data
+          ArrayList<Integer> columnPositions = new ArrayList<>();
+          ArrayList<Integer> tmpPositions;
+          for(int i = 0; i < this.BMFiles.length; i++) {
+            tmpPositions = (Bitmap_scan) IndexUtils.Bitmap_scan(this.BMFiles[i], indName[i]);
+            for(int pos: tmpPositions) {
+              columnPositions.add(pos);
+            }
+          }
+          // Remove duplicates
+          this.distinctColPos = new ArrayList<>();
+          this.distinctColPos = this.removeDuplicates(columnPositions);
+          Collections.sort(this.distinctColPos);
+
+          this.scanIndex = 0;
+          this.tidHeapFile = columnarFile.tidHeap.openScan();
+        } catch (Exception e) {
+          throw new IndexException(
+              e, "IndexScan.java: BTreeFile exceptions caught from IndexUtils.BTree_scan().");
+        }
+
+        break;
+      case IndexType.None:
+      default:
+        throw new UnknownIndexTypeException("Only BTree index is supported so far");
+    }
+  }
+
+  /**
+   * returns the next tuple. if <code>index_only</code>, only returns the key value (as the first
+   * field in a tuple) otherwise, retrive the tuple and returns the whole tuple
+   *
+   * @return the tuple
+   * @exception IndexException error from the lower layer
+   * @exception UnknownKeyTypeException key type unknown
+   * @exception IOException from the lower layer
+   */
+  public Tuple get_next() throws IndexException, UnknownKeyTypeException, IOException {
+    RID rid = null;
+    Tuple tidTuple;
+    byte[] byteArray;
+    int unused;
+
+
+    if(this.scanIndex < this.distinctColPos) {
+      // Traverse tidHeapFile
+      while(tidTuple = tidHeapfile.getNext(rid) != null) {
+        try {
+          // byteArray stores target byteArray
+          byteArray = tidTuple.getTupleByteArray();
+          tid = new TID(byteArray);
+        } catch (Exception e) {
+          throw new IndexException(e, "ColumnarIndexScan.java: getTID failed");
+        }
+        if(tid.poition == this.distinctColPos.get(this.scanIndex)) {
+          try {
+            tuple1 = columnarFile.getTuple(tid);
+          } catch (Exception e) {
+            throw new IndexException(e, "ColumnarIndexScan.java: getRecord failed");
+          }
+          try {
+            tuple1.setHdr((short) _noInFlds, _types, _s_sizes);
+          } catch (Exception e) {
+            throw new IndexException(e, "ColumnarIndexScan.java: Heapfile error");
+          }
+
+          boolean eval;
+          try {
+            eval = PredEval.Eval(_selects, tuple1, null, _types, null);
+          } catch (Exception e) {
+            throw new IndexException(e, "ColumnarIndexScan.java: Heapfile error");
+          }
+
+          if (eval) {
+            // need projection.java
+            try {
+              Projection.Project(tuple1, _types, Jtuple, perm_mat, _noOutFlds);
+            } catch (Exception e) {
+              throw new IndexException(e, "ColumnarIndexScan.java: Heapfile error");
+            }
+
+            return Jtuple;
+          }
+        }
+      }
+
+      try {
+       this.scanIndex ++;
+      } catch (Exception e) {
+        throw new IndexException(e, "ColumnarIndexScan.java: BitMap error");
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Cleaning up the index scan, does not remove either the original relation or the index from the
+   * database.
+   *
+   * @exception IndexException error from the lower layer
+   * @exception IOException from the lower layer
+   */
+  public void close() throws IOException, IndexException {
+    try {
+      this.tidHeapFile.closetuplescan();
+    } catch (Exception e) {
+      throw new IndexException(e, "BTree error in destroying index scan.");
+    }
+  }
+
+  private static ArrayList<Integer> removeDuplicates(ArrayList<Integer> arrayList) {
+    Set<Integer> set = new HashSet<>(arrayList);
+    return new ArrayList<>(set);
+  }
+}
