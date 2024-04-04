6e309932a5a07c15aef36c84964fb49a34693155
StevenChiu2018 <sam.200846@gmail.com>
2024-04-03 17:32:36 -0700

    Speedup index

--- src/programs/ProgramIndex.java
+++ src/programs/ProgramIndex.java
@@ -1,8 +1,8 @@
 package programs;
 
 import java.io.IOException;
+import java.util.HashSet;
 import bitmap.AddFileEntryException;
-import bitmap.BitMapFile;
 import bitmap.ConstructPageException;
 import bitmap.GetFileEntryException;
 import bitmap.PinPageException;
@@ -19,6 +19,7 @@ import bufmgr.ReplacerException;
 import columnar.ColumnInfo;
 import columnar.Columnarfile;
 import diskmgr.Pcounter;
+import global.AttrType;
 import global.ByteValue;
 import global.Convert;
 import global.RID;
@@ -26,13 +27,12 @@ import global.SystemDefs;
 import heap.HFBufMgrException;
 import heap.HFDiskMgrException;
 import heap.HFException;
-import heap.Heapfile;
 import heap.InvalidSlotNumberException;
 import heap.InvalidTupleSizeException;
 import heap.Scan;
 import heap.SpaceNotAvailableException;
 import heap.Tuple;
-import index.ColumnIndexScan;
+
 
 public class ProgramIndex {
     public static void main(String[] args)
@@ -111,15 +111,30 @@ public class ProgramIndex {
         Columnarfile columnarFile = new Columnarfile(columnarFileName);
         ColumnInfo columnInfo = columnarFile.getColumnInfoByColumnName(columnName);
         Scan columnScan = columnarFile.columns[columnInfo.columnNo].openScan();
+
         Tuple value;
         RID rid = new RID();
-
         int count = 0;
+        HashSet<String> insertedIndexKey = new HashSet<String>();
         Pcounter.initialize();
         while ((value = columnScan.getNext(rid)) != null) {
-            ByteValue byteVaule = new ByteValue(value.getTupleByteArray(), columnInfo.type.attrType,
-                    columnInfo.sizeInBytes);
-            columnarFile.createBitMapIndex(columnInfo.columnNo, byteVaule);
+            byte[] trueValueByte = value.getTupleByteArray();
+            String trueValueString = "";
+
+            if (columnInfo.type.attrType == AttrType.attrInteger) {
+                int intValue = Convert.getIntValue(0, trueValueByte);
+                trueValueString = Integer.toString(intValue);
+            } else {
+                trueValueString = Convert.getStrValue(0, trueValueByte, columnInfo.sizeInBytes);
+            }
+
+            if (!insertedIndexKey.contains(trueValueString)) {
+                ByteValue byteVaule = new ByteValue(trueValueByte, columnInfo.type.attrType,
+                        columnInfo.sizeInBytes);
+                columnarFile.createBitMapIndex(columnInfo.columnNo, byteVaule);
+                insertedIndexKey.add(trueValueString);
+            }
+
             System.out.print("The " + count++ + "th key is inserted to bitmap\r");
         }
 
