da852975088ea6da76e0177434b202c4a6235a49
Max <37968101+Max851010@users.noreply.github.com>
2024-04-12 09:54:51 -0700

    feat: Add exception handlers in cbitmap package (#27)

---
+++ src/cbitmap/GetFileEntryException.java
@@ -0,0 +1,17 @@
+package cbitmap;
+
+import chainexception.*;
+
+public class GetFileEntryException extends ChainException {
+  public GetFileEntryException() {
+    super();
+  }
+
+  public GetFileEntryException(String s) {
+    super(null, s);
+  }
+
+  public GetFileEntryException(Exception e, String s) {
+    super(e, s);
+  }
+}
